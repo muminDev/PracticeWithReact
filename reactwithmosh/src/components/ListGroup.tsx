e71d3f97478cac31db004223f77ef1259f641dfb
Muminjon <juramirzaev.muminjon@gmail.com>
2023-11-18 09:15:26 +0100

    got useState hook

--- reactwithmosh/src/components/ListGroup.tsx
+++ reactwithmosh/src/components/ListGroup.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function Listgroup() {
   const cities = ["Ottava", "Berlin", "Munich", "Warsaw", "Paris"];
-
+  const [selectedItem, setSelectedItem] = useState(-1);
   const renderedList = cities.map((item, index) => (
-    <li key={index} className="list-group-item">
+    <li
+      key={index}
+      className={
+        selectedItem === index ? "list-group-item active" : "list-group-item"
+      }
+      onClick={() => {
+        setSelectedItem(index);
+      }}
+    >
       {item}
     </li>
   ));
@@ -10,6 +20,7 @@ function Listgroup() {
   return (
     <>
       <h1>Cities</h1>
+      {cities.length === 0 && <p>no items found</p>}
       <ul className="list-group">{renderedList}</ul>
     </>
   );
